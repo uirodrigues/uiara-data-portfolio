85bf4656994c273bae7b148ae247687799c72d99
uirodrigues <uiaratelesrodrigues@gmail.com>
2026-03-18 16:50:45 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Homepage from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Homepage />
-  </React.StrictMode>
-);

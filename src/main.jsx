e3d7fff20b859ab06aa20e368863f79d6a6c7808
uirodrigues <uiaratelesrodrigues@gmail.com>
2026-03-20 10:48:35 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Garante que o elemento root existe antes de renderizar
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element. Make sure there is a <div id='root'></div> in your HTML.");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
